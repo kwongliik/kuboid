8030fd90d06bbb0e59529216a36d2a476b32d041
kwongliik <46083661+kwongliik@users.noreply.github.com>
2023-10-25 22:48:09 +0800

    Update kuboid.py 1
    create bugs

--- kuboid.py
+++ kuboid.py
@@ -1,13 +1,14 @@
-def kira_kuboid(panjang, lebar, tinggi):
+def kira_kuboid(a, b, c):
     isipadu_kuboid = panjang * lebar * tinggi
-    return isipadu_kuboid
+    return isipadu
 
 def main():
     a = float(input("Masukkan panjang: "))
     b = float(input("Masukkan lebar: "))
-    c = float(input("Masukkan tinggi: "))
-    isipadu_kuboid = kira_kuboid(a, b, c)
-    print(f"Isipadu kuboid = {isipadu_kuboid}")
+    c = float(input("Masukkan tinggi: ")
+    isipadu_kuboid = kuboid(a, b, c)
+    print(f"Isipadu kuboid = {isipadu}")
 
+# JANGAN ubah kod di bawah
 if __name__ == "__main__":
     main()
