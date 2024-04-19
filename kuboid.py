4d88f2b01f44a32dc7f2a88f43d09bbb698dabc5
kwongliik <46083661+kwongliik@users.noreply.github.com>
2024-04-19 12:05:42 +0800

    ralat 2

--- kuboid.py
+++ kuboid.py
@@ -2,7 +2,7 @@ def kuboid():
     a = float(input("Masukkan panjang: "))
     b = float(input("Masukkan lebar: ")
     c = float(input("Masukkan tinggi: "))
-    isipadu_kuboid = A * b / c
+    isipadu_kuboid = A * b / C
     print(f"Isipadu kuboid = {isipadu")
 
 # JANGAN ubah kod di bawah
